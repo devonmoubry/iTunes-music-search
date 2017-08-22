6c6ceb05369bc9ce875c6bc7b9565d3c9f124f91
Devon Moubry <devon@moubry.com>
2017-08-22 14:33:20 -0500

    beautify

--- main.js
+++ main.js
@@ -3,26 +3,25 @@
 */
 
 // 1. First select and store the elements you'll be working with
-let url =                         'https://itunes.apple.com/search?term=';
-let searchForm =                  document.querySelector('.search-form');
-let searchInput =                 document.querySelector('.search-input');
-let searchButton =                document.querySelector('#search-button');
-let resultsContainer =            document.querySelector('.results');
-let searchValue =                 '';
-let searchResultsHeading =        document.createElement('div');
-let searchResultsBody =           document.createElement('div');
-searchResultsHeading.setAttribute('class', 'results-heading');
-searchResultsBody.setAttribute('class', 'results-body');
-
+let url = "https://itunes.apple.com/search?term=";
+let searchForm = document.querySelector(".search-form");
+let searchInput = document.querySelector(".search-input");
+let searchButton = document.querySelector("#search-button");
+let resultsContainer = document.querySelector(".results");
+let searchValue = "";
+let searchResultsHeading = document.createElement("div");
+let searchResultsBody = document.createElement("div");
+searchResultsHeading.setAttribute("class", "results-heading");
+searchResultsBody.setAttribute("class", "results-body");
 
 // 2. Create your `submit` event for getting the user's search term
-searchForm.addEventListener('submit', (e) => {
+searchForm.addEventListener("submit", e => {
   searchValue = e.target[0].value;
   e.preventDefault();
-  console.log('hello');
+  console.log("hello");
   console.log(searchValue);
-// 3. Create your `fetch` request that is called after a submission
-  fetch( url + searchValue )
+  // 3. Create your `fetch` request that is called after a submission
+  fetch(url + searchValue)
     .then(function(response) {
       if (response.status !== 200) {
         console.log(response.status);
@@ -32,24 +31,23 @@ searchForm.addEventListener('submit', (e) => {
         console.log("test", response.url);
         // 4. Create a way to append the fetch results to your page
         let results = data.results;
-        let resultContainer = '';
-        results.forEach( result => {
+        let resultContainer = "";
+        results.forEach(result => {
           resultContainer += `
             <div class="result">
               <img src="${result.artworkUrl100}" alt="${result.trackName} picture" title="${result.previewUrl}">
               <p class="song">${result.trackName}</p>
               <p class="artist">${result.artistName}</p>
             </div>
-          `
+          `;
         }); // results.forEach()
 
-       searchResultsMessage = `<h4>Showing results for '${searchValue}'...</h1><hr>`;
-       searchResultsHeading.innerHTML =  searchResultsMessage;
-       searchResultsBody.innerHTML = resultContainer;
-       resultsContainer.append(searchResultsBody);
+        searchResultsMessage = `<h4>Showing results for '${searchValue}'...</h1><hr>`;
+        searchResultsHeading.innerHTML = searchResultsMessage;
+        searchResultsBody.innerHTML = resultContainer;
+        resultsContainer.append(searchResultsBody);
       }); // response.json().then
     }) // .then()
-
     .catch(function(err) {
       console.log("Fetch Error :-S", err);
     }); // .catch()
@@ -58,10 +56,10 @@ searchForm.addEventListener('submit', (e) => {
 }); // searchForm.addEventListener()
 
 // 5. Create a way to listen for a click that will play the song in the audio play
-searchResultsBody.addEventListener('click', (e) => {
+searchResultsBody.addEventListener("click", e => {
   e.preventDefault();
-  let audioSource = document.getElementsByTagName('source');
-  let audio =       document.querySelector('audio');
-  audioSource[0].setAttribute('src', e.target.title);
+  let audioSource = document.getElementsByTagName("source");
+  let audio = document.querySelector("audio");
+  audioSource[0].setAttribute("src", e.target.title);
   audio.load();
 }); // searchResultsBody.addEventListener()
